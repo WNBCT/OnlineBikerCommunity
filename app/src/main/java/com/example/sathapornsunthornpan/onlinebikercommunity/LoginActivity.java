6811a1c3cf04329ade91694aa88f49b56ae64f88
stitch <dd.stitch55@gmail.com>
2016-12-02 17:40:06 +0700

    add Activity Login

--- app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/LoginActivity.java
+++ app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/LoginActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+
+
     }
 }
--- app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/MainActivity.java
+++ app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sathapornsunthornpan.onlinebikercommunity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
