cd4c712855ef49cf1558affae8cb9b246dd2ddb5
Tis Sathaporn S <dd.stitch55@gmail.com>
2016-12-16 00:12:46 +0700

    feed news ktm

---
+++ app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/news/FeedModel.java
@@ -0,0 +1,5 @@
+package com.example.sathapornsunthornpan.onlinebikercommunity.news;
+
+
+public class FeedModel {
+}
---
+++ app/src/main/java/com/example/sathapornsunthornpan/onlinebikercommunity/news/NewsFeedModel.java
@@ -0,0 +1,8 @@
+package com.example.sathapornsunthornpan.onlinebikercommunity.news;
+
+/**
+ * Created by sathapornsunthornpan on 12/16/16.
+ */
+
+public class NewsFeedModel {
+}
